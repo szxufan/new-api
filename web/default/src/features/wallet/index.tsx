8722873068ba3adcbba6273ccbadb32c2c1def22
CaIon <i@caion.me>
2026-05-26 12:03:02 +0800

    feat(subscription): support balance purchases
    Refs #3071.

--- web/default/src/features/wallet/index.tsx
+++ web/default/src/features/wallet/index.tsx
@@ -312,6 +312,8 @@ export function Wallet(props: WalletProps) {
               <SubscriptionPlansCard
                 topupInfo={topupInfo}
                 onAvailabilityChange={handleSubscriptionAvailabilityChange}
+                userQuota={user?.quota}
+                onPurchaseSuccess={fetchUser}
               />
             </div>
 
